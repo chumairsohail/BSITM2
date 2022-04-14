0af6d3166478d1387b6ff492cfaab237b203186e
chumairsohail <umairsohail887@gmail.com>
2022-04-14 13:57:36 +0500

    THREADING ASSIGNMENT OS

---
+++ OS/Assignments/threading in cpp/threading in cpp.cpp
@@ -0,0 +1,23 @@
+// BSIT M2-20-35 (umair sohail)
+#include <Windows.h>
+#include <process.h>
+#include <stdio.h>
+
+unsigned int __stdcall mythread(void* data) 
+{
+	printf("Thread %d\n", GetCurrentThreadId());
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	HANDLE myhandle;
+
+	myhandle = (HANDLE)_beginthreadex(0, 0, &mythread, 0, 0, 0);
+
+	WaitForSingleObject(myhandle, INFINITE);
+
+	CloseHandle(myhandle);
+
+	return 0;
+}
